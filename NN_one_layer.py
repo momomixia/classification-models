b9fbcb7200b9140b99a32bcde2939d650ee6d937
fubao <freadream@gmail.com>
2017-10-18 17:40:01 -0400

    update_function

--- NN_one_layer.py
+++ NN_one_layer.py
@@ -20,30 +20,6 @@ def feedForward(W, b, V, c, train_x):
     out = np.dot(hid, V) + c
     return out
 
-'''
-#mean logistic loss
-def mean_logistic_loss(weights, x, y, unflatten):
-      # regularization penalty
-    lambda_pen = 10
-
-    # unflatten weights into W, b, V and c respectively 
-    (W, b, V, c) = unflatten(weights)
-
-    # Predict output for the entire train data
-    out  = feedForward(W, b, V, c, x)
-    #pred = np.argmax(out, axis=1)
-
-	# True labels
-    #true = np.argmax(y, axis=1)
-    # Mean accuracy
-    #class_err = np.mean(pred != true)
-
-    # Computing logistic loss with l2 penalization
-    logistic_loss = np.sum(-np.sum(out * y, axis=1) + np.log(np.sum(np.exp(out),axis=1))) + lambda_pen * np.sum(weights**2)
-    #logistic_loss = np.sum(np.sum(np.log(1+np.exp(-1*out*y)), axis=1)) #+ lambda_pen * np.sum(weights**2)
-    #print ("x. shape:" , logistic_loss, x.shape[0], autograd.util.getval(logistic_loss), logistic_loss/x.shape[0])
-    return logistic_loss/x.shape[0]       #np.mean(logistic_loss)
-'''
     
 # Logistic Loss function
 def logistic_loss_batch(weights, x, y, unflatten):
@@ -253,6 +229,6 @@ def stratifyDataTrainTestNN():
 #main entry function
 if __name__== "__main__":
     print ("begin train whole data and test plot and time ... for b. (1) (2)")
-    #nnOneLayerTrainEntry()               # for plot
+    nnOneLayerTrainEntry()               # for plot
     print ("begin stratified data traing and test for b. (3)")
     stratifyDataTrainTestNN()
